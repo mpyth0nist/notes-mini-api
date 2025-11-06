618114cb7fbb65498e296842200f53c5bdc8f95a
maliki <devossama9@gmail.com>
2025-11-06 20:54:11 +0100

    Refactor notes service methods for consistency and clarity and implemented delete notes method.

--- services/notes.service.js
+++ services/notes.service.js
@@ -1,12 +1,47 @@
-const Notes = require('../models/notes.model')
+const Note = require('../models/notes.model')
 
 
-exports.fetchAll = () => {
-    const notes = Notes.find({})
+exports.getAllNotes = () => {
+    const notes = Note.find({})
     return notes;
 }
 
-exports.fetchById = (id) => {
-    const note = Notes.findById(id);
+exports.getNoteById = (id) => {
+    const note = Note.findById(id);
     if (!note) throw new Error('Note not found')
+    return note
+}
+
+exports.createNewNote = (data) => {
+    const newNote = new Note({
+        title: data.title,
+        content : data.content,
+        author : data.author,
+        tags: data.tags,
+        createdAt: data.createdAt
+        
+    })
+}
+
+exports.updateNote = async (noteId, newData) => {
+    let oldNote = await this.getNoteById(noteId).toObject()
+    Object.keys(newData).forEach(key => {
+        if(Object.hasOwn(oldNote, key)){
+            oldNote[key] = newData[key]
+        }
+    })
+
+    const updatedNote = await oldNote.save()
+
+    if(!updatedNote) throw new Error('Failed to update note')
+    
+    return updatedNote
+}
+
+exports.deleteNote = async (id) => {
+    const deleted = await Note.findByIdAndDelete(id)
+
+    if(!deleted) throw new Error('Note does not exist')
+    
+    return deleted;
 }
